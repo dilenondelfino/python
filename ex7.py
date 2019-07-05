b15f78abe22390800534af8ebf12781cbbd4606a
Dilenon Delfino <stefan.dilenon@gmail.com>
2019-07-04 21:36:45 -0300

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,26 @@
+my_string = input("Insira a sua palavra: ")
+
+my_string.lower()
+
+if 'a' in my_string or 'e' in my_string or 'i' in my_string or \
+    'o' in my_string or 'u' in my_string:
+    print("A palavra tem vogais")
+
+if 'a' in my_string:
+    print("A palavra tem A")
+
+if 'e' in my_string:
+    print("A palavra tem E")
+
+if 'o' in my_string:
+    print("A palavra tem I")
+
+if 'i' in my_string:
+    print("A palavra tem O")
+
+if 'u' in my_string:
+    print("A palavra tem U")
+
+if ' ' in my_string:
+    print("Isso é uma frase")
+
